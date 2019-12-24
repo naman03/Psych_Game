f46a6c194c999a77ee9ef7e51f49833b2bfd6d12
naman03 <naman.ruhela3@gmail.com>
2019-12-24 12:48:57 +0530

    Basic template for hibernate + models + REST controllers

---
+++ src/main/java/com/psych/game/model/Employee.java
@@ -0,0 +1,38 @@
+package com.psych.game.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "employees")
+public class Employee  extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    @NotBlank
+    private String name;
+
+    @Getter
+    @Setter
+    @NotBlank
+    @Email
+    private String email;
+
+}
